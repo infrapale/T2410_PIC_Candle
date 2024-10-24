e2bbb10793240e2621b92d0a9fc50407f4c230ff
infrapale <infrapale@gmail.com>
2024-10-24 19:28:55 +0300

    duty ratio limited to 2

--- main_candle.c
+++ main_candle.c
@@ -36,11 +36,11 @@ const uint16_t bit_mask[PWM_NBR_BIT] =
     0x0100,0x0200,0x0400,0x0800,0x1000,0x2000,0x4000,0x8000
 };
 
-const uint8_t led_iter[NBR_OF_LED] = {10,6,4,4,5,6,3,2};
+uint8_t led_iter[NBR_OF_LED] = {10,6,4,20,5,6,3,2};
 const uint8_t wave[WAVE_LEN] = 
 {
-    0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,
-    15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0
+    2,2,3,3,4,5,6,7,8,9,10,11,12,13,14,15,
+    15,14,13,12,11,10,9,8,7,6,5,4,3,3,2,2
 };
 uint8_t     led_pos[NBR_OF_LED] = {0, 8, 10, 12, 14, 16, 20, 30};
 uint8_t     led_iter_cntr[NBR_OF_LED] = {0};
@@ -72,7 +72,7 @@ int main()
               led_pos[lindex]++;
               if (led_pos[lindex] >= WAVE_LEN ) led_pos[lindex] = 0;
           }
-          
+           
           led_pattern[lindex] = duty_ratio[wave[led_pos[lindex]]];
       }
       for (uint8_t bindex = 0; bindex < PWM_NBR_BIT; bindex++ )
